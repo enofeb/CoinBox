f42e3c908092ebe753aa794ff3486496a3327097
Enes Zor <enes.zor@leanscale.com>
2021-07-09 13:48:32 +0300

    Splash fragment was added

--- app/src/main/java/com/enofeb/coinbox/splash/SplashActivity.kt
+++ app/src/main/java/com/enofeb/coinbox/splash/SplashActivity.kt
@@ -2,13 +2,16 @@ package com.enofeb.coinbox.splash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.enofeb.coinbox.R
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
+//Will be done with Compose
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
         runBlocking {
             delay(100)
         }
---
+++ app/src/main/java/com/enofeb/coinbox/splash/SplashFragment.kt
@@ -0,0 +1,8 @@
+package com.enofeb.coinbox.splash
+
+import androidx.fragment.app.Fragment
+
+//Will be done with Compose
+class SplashFragment : Fragment() {
+//no-op
+}

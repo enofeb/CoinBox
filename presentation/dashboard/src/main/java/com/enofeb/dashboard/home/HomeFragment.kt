f273172f033d264ef66563b0f7892061799b1305
Enes Zor <enes.zor@leanscale.com>
2021-07-10 17:59:42 +0300

    Home fragment was created with graph

---
+++ presentation/dashboard/src/main/java/com/enofeb/dashboard/home/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.enofeb.dashboard.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setContent {
+            Text("This is home fragment!")
+        }
+    }
+
+}

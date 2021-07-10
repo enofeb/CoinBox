9bb3d9d24d63b2ef9830560690ce7dde6afd099c
Enes Zor <enes.zor@leanscale.com>
2021-07-10 18:11:43 +0300

    Price fragment was added

---
+++ presentation/price/src/main/java/com/enofeb/price/PriceFragment.kt
@@ -0,0 +1,21 @@
+package com.enofeb.price
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class PriceFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setContent {
+            Text("This is price fragment!")
+        }
+    }
+}

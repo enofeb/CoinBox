213ca1cf59f96fb400ee81bfbf7add76ebaebaf9
Enes Zor <enes.zor@leanscale.com>
2021-07-10 12:29:28 +0300

    Dashboard navigation issue was fixed

--- app/src/main/java/com/enofeb/coinbox/splash/SplashActivity.kt
+++ app/src/main/java/com/enofeb/coinbox/splash/SplashActivity.kt
@@ -1,19 +1,60 @@
 package com.enofeb.coinbox.splash
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.enofeb.coinbox.R
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.*
 
-//Will be done with Compose
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : ComponentActivity() {
+
+    private val splashScope = CoroutineScope(Dispatchers.Main) + SupervisorJob()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        runBlocking {
-            delay(100)
+        setContent {
+            MaterialTheme {
+                InitialView()
+                splashScope.launch {
+                    delay(3000)
+                    navigateToHome()
+                }
+            }
         }
     }
+
+    override fun onPause() {
+        splashScope.cancel()
+        super.onPause()
+    }
+
+    private fun navigateToHome() {
+        try {
+            val intent = Intent(
+                this,
+                Class.forName("com.enofeb.dashboard.MainActivity")
+            )
+            startActivity(intent)
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+        }
+    }
+}
+
+@Composable
+fun InitialView() {
+    Row {
+        Text(text = "CoinBox")
+    }
+}
+
+@Preview
+@Composable
+fun WatchPreview() {
+    InitialView()
 }

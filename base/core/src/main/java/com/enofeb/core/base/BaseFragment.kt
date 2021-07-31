4c2340c69d09ed335ebd1af1db544b41da033851
Enes Zor <eneszor95@gmail.com>
2021-07-31 12:18:11 +0300

    Base fragment was updated

--- base/core/src/main/java/com/enofeb/core/base/BaseFragment.kt
+++ base/core/src/main/java/com/enofeb/core/base/BaseFragment.kt
@@ -1,9 +1,18 @@
 package com.enofeb.core.base
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.enofeb.core.state.UiState
 import com.enofeb.core.state.UiStateRender
+import com.enofeb.core.state.intent.UiIntent
 
-abstract class BaseFragment<US : UiState> : Fragment(), UiStateRender<US> {
+abstract class BaseFragment<UI : UiIntent, US : UiState, VM : BaseViewModel<UI, US>>(val viewModelClass: Class<VM>) :
+    Fragment(),
+    UiStateRender<US> {
 
+    private val _viewModel by lazy {
+        ViewModelProvider(this).get(viewModelClass.kotlin.java)
+    }
 }

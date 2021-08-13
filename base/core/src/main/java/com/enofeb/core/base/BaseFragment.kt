823f9fa5a61ca828feac48f40ca853d69ac4867d
Enes Zor <eneszor95@gmail.com>
2021-08-13 16:13:24 +0300

    Theme was updated

--- base/core/src/main/java/com/enofeb/core/base/BaseFragment.kt
+++ base/core/src/main/java/com/enofeb/core/base/BaseFragment.kt
@@ -1,24 +1,20 @@
 package com.enofeb.core.base
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.enofeb.core.state.UiState
 import com.enofeb.core.state.UiStateRender
 import com.enofeb.core.state.intent.UiIntent
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
+import com.enofeb.core.ui.theme.CoinBoxTheme
 
 abstract class BaseFragment<UI : UiIntent, US : UiState, VM : BaseViewModel<UI, US>>(private val viewModelClass: Class<VM>) :
     Fragment(),
@@ -34,7 +30,7 @@ abstract class BaseFragment<UI : UiIntent, US : UiState, VM : BaseViewModel<UI, 
         savedInstanceState: Bundle?
     ): View = ComposeView(requireContext()).apply {
         setContent {
-            Column {
+            ComposeMagic {
                 DrawScreen()
                 ObserveDemo()
             }
@@ -46,6 +42,15 @@ abstract class BaseFragment<UI : UiIntent, US : UiState, VM : BaseViewModel<UI, 
     private fun ObserveDemo() {
         val viewState = viewModel.uiState.collectAsState().value
         Render(viewState)
+    }
+
+    @Composable
+    private fun ComposeMagic(content: @Composable () -> Unit) {
+        CoinBoxTheme(darkTheme = true) {
+            Surface(color = Color.Black) {
+                content()
+            }
+        }
     }
 
     @Composable

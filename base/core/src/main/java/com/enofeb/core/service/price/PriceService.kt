763e9e6aaf3728027308a54dce19435c35d6fa6f
Enes Zor <eneszor95@gmail.com>
2021-08-14 19:47:37 +0300

    Price service was updated

--- base/core/src/main/java/com/enofeb/core/service/price/PriceService.kt
+++ base/core/src/main/java/com/enofeb/core/service/price/PriceService.kt
@@ -2,12 +2,10 @@ package com.enofeb.core.service.price
 
 import com.enofeb.core.constants.ApiConstants.Companion.API_PREFIX
 import com.enofeb.core.constants.ApiConstants.Companion.API_VERSION
-import com.enofeb.core.data.price.Coin
-import com.enofeb.core.data.price.CurrencyType
 import retrofit2.http.GET
-import retrofit2.http.Query
-import java.util.*
 
 interface PriceService {
-    //no-op
+
+    @GET("${API_PREFIX}${API_VERSION}/exchangeRates")
+    suspend fun getExchangeRates()
 }

bae89e532fe6b2530d82806620cd97435371b577
Enes Zor <eneszor95@gmail.com>
2021-08-14 16:39:57 +0300

    Home fragment was updated

--- presentation/dashboard/src/main/java/com/enofeb/dashboard/home/HomeFragment.kt
+++ presentation/dashboard/src/main/java/com/enofeb/dashboard/home/HomeFragment.kt
@@ -2,10 +2,7 @@ package com.enofeb.dashboard.home
 
 import android.os.Bundle
 import android.view.View
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
@@ -13,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LastBaseline
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.enofeb.core.base.BaseFragment
@@ -72,8 +70,9 @@ fun CoinItem(coin: Coin) {
             .padding(15.dp), elevation = 10.dp,
         backgroundColor = Color.Black
     ) {
-        Column(modifier = Modifier.padding(15.dp)) {
+        Row(modifier = Modifier.padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween) {
             Text(text = coin.name, color = Color.White)
+            Text(text = coin.currentPrice.toString(), color = Color.White)
         }
     }
 }
@@ -81,9 +80,16 @@ fun CoinItem(coin: Coin) {
 @Preview
 @Composable
 fun ItemPreview() {
-    Row {
-        Column(modifier = Modifier.padding(15.dp)) {
-            Text(text = "Bitcoin", color = Color.White)
+    Card(
+        modifier =
+        Modifier
+            .fillMaxWidth()
+            .padding(15.dp), elevation = 10.dp,
+        backgroundColor = Color.Black
+    ) {
+        Row(modifier = Modifier.padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(text = "Eth", color = Color.White)
+            Text(text = "3224.5", color = Color.White)
         }
     }
 }

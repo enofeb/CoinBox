c29d3f1f0cf7b05a0f9614f55c6e5c6746fede85
Enes Zor <eneszor95@gmail.com>
2021-08-14 16:53:33 +0300

    Coin card view was updated

--- presentation/dashboard/src/main/java/com/enofeb/dashboard/home/HomeFragment.kt
+++ presentation/dashboard/src/main/java/com/enofeb/dashboard/home/HomeFragment.kt
@@ -2,17 +2,20 @@ package com.enofeb.dashboard.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.LastBaseline
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.enofeb.core.base.BaseFragment
 import com.enofeb.core.data.price.Coin
 import dagger.hilt.android.AndroidEntryPoint
@@ -71,7 +74,20 @@ fun CoinItem(coin: Coin) {
         backgroundColor = Color.Black
     ) {
         Row(modifier = Modifier.padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-            Text(text = coin.name, color = Color.White)
+            Row {
+                Image(
+                    painter = rememberImagePainter(coin.imageUrl),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(15.dp, 15.dp)
+                        .align(Alignment.CenterVertically)
+                )
+                Text(
+                    text = coin.name,
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 10.dp)
+                )
+            }
             Text(text = coin.currentPrice.toString(), color = Color.White)
         }
     }
@@ -88,7 +104,14 @@ fun ItemPreview() {
         backgroundColor = Color.Black
     ) {
         Row(modifier = Modifier.padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-            Text(text = "Eth", color = Color.White)
+            Row {
+                Image(
+                    painter = rememberImagePainter("https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880"),
+                    contentDescription = null,
+                    Modifier.size(10.dp, 10.dp)
+                )
+                Text(text = "Eth", color = Color.White)
+            }
             Text(text = "3224.5", color = Color.White)
         }
     }

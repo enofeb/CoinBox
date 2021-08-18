152bcff202352262e78da4edc77bfab3a95315cc
Enes Zor <eneszor95@gmail.com>
2021-08-18 21:11:36 +0300

    ExchangeRate was updated

--- base/core/src/main/java/com/enofeb/core/data/price/exchange/ExchangeRate.kt
+++ base/core/src/main/java/com/enofeb/core/data/price/exchange/ExchangeRate.kt
@@ -3,5 +3,8 @@ package com.enofeb.core.data.price.exchange
 import com.google.gson.annotations.SerializedName
 
 data class ExchangeRate(
-    @SerializedName("name") val name: String?
+    @SerializedName("name") val name: String?,
+    @SerializedName("unit") val unit: String?,
+    @SerializedName("value") val value: Double?,
+    @SerializedName("type") val type: String?
 )

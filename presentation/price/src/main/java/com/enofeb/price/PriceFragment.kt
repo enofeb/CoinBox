9bcd41ca226486a8968716ba430c880d0c2ce9a3
Enes Zor <eneszor95@gmail.com>
2021-08-21 16:48:29 +0300

    Currency dropdown was added

--- presentation/price/src/main/java/com/enofeb/price/PriceFragment.kt
+++ presentation/price/src/main/java/com/enofeb/price/PriceFragment.kt
@@ -2,19 +2,25 @@ package com.enofeb.price
 
 import android.os.Bundle
 import android.view.View
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import com.enofeb.core.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import kotlin.math.exp
 
 @AndroidEntryPoint
 class PriceFragment :
@@ -45,8 +51,14 @@ class PriceFragment :
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            SellTextField()
-            BuyTextField()
+            Row {
+                SellTextField()
+                CurrencyDropDown()
+            }
+            Row{
+                BuyTextField()
+                CurrencyDropDown()
+            }
         }
     }
 }
@@ -71,6 +83,42 @@ fun BuyTextField() {
         onValueChange = { value = it },
         label = { Text("Buy") }
     )
+}
+
+@Composable
+fun CurrencyDropDown() {
+    var expanded by remember { mutableStateOf(false) }
+
+    val currencies = listOf("BTC", "ETH", "SOL")
+
+    var selectedText by remember { mutableStateOf("") }
+
+    val icon = if (expanded) {
+        Icons.Filled.KeyboardArrowUp
+    } else {
+        Icons.Filled.KeyboardArrowDown
+    }
+
+    Column {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = { selectedText = it },
+            label = { Text("Name") },
+            trailingIcon = {
+                Icon(icon, "contentDescription",
+                    Modifier.clickable { expanded = !expanded })
+            },
+            readOnly = true
+        )
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            currencies.forEach { label ->
+                DropdownMenuItem(onClick = { selectedText = label }) {
+                    Text(text = label)
+                }
+            }
+        }
+    }
+
 }
 
 @Preview

61c745261dc946563d67b9f442601db674716ee5
Enes Zor <eneszor95@gmail.com>
2021-08-29 21:26:24 +0300

    Popular endpoint api was added

--- base/core/src/main/java/com/enofeb/core/service/market/MarketService.kt
+++ base/core/src/main/java/com/enofeb/core/service/market/MarketService.kt
@@ -3,6 +3,7 @@ package com.enofeb.core.service.market
 import com.enofeb.core.constants.ApiConstants
 import com.enofeb.core.data.market.Coin
 import com.enofeb.core.data.market.CurrencyType
+import com.enofeb.core.data.market.popular.PopularCoinResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -10,4 +11,7 @@ interface MarketService {
 
     @GET("${ApiConstants.API_PREFIX}${ApiConstants.API_VERSION}/coins/markets")
     suspend fun getCoinMarket(@Query("vs_currency") vsCurrency: String? = CurrencyType.USD.shortName): List<Coin>?
+
+    @GET("${ApiConstants.API_PREFIX}${ApiConstants.API_VERSION}/search/trending")
+    suspend fun getPopularCoin(): PopularCoinResponse
 }

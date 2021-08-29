f48049bccd508cbbdb5c217b7de895778fa3a6b8
Enes Zor <eneszor95@gmail.com>
2021-08-29 21:27:37 +0300

    Popular endpoint api was added in repo domain

--- base/core/src/main/java/com/enofeb/core/domain/market/MarketRepository.kt
+++ base/core/src/main/java/com/enofeb/core/domain/market/MarketRepository.kt
@@ -1,9 +1,12 @@
 package com.enofeb.core.domain.market
 
 import com.enofeb.core.data.market.Coin
+import com.enofeb.core.data.market.popular.PopularCoinResponse
 import com.enofeb.core.domain.Repository
 import kotlinx.coroutines.flow.Flow
 
 interface MarketRepository : Repository {
     fun getCoinMarket(): Flow<List<Coin>?>
+
+    fun getPopularCoins(): Flow<PopularCoinResponse?>
 }
--- base/core/src/main/java/com/enofeb/core/domain/market/MarketRepositoryImpl.kt
+++ base/core/src/main/java/com/enofeb/core/domain/market/MarketRepositoryImpl.kt
@@ -1,14 +1,22 @@
 package com.enofeb.core.domain.market
 
 import com.enofeb.core.data.market.Coin
+import com.enofeb.core.data.market.popular.PopularCoinResponse
 import com.enofeb.core.extensions.knock
 import com.enofeb.core.service.market.MarketService
 import kotlinx.coroutines.flow.Flow
 
 class MarketRepositoryImpl(private val marketService: MarketService) : MarketRepository {
+
     override fun getCoinMarket(): Flow<List<Coin>?> {
         return knock {
             marketService.getCoinMarket()
+        }
+    }
+
+    override fun getPopularCoins(): Flow<PopularCoinResponse?> {
+        return knock {
+            marketService.getPopularCoin()
         }
     }
 }
